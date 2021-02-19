6f5e7c89703c49520b8e46e2d6048854bda0e2db
SprintsMcGee <joshua.w.bennett01@gmail.com>
2021-02-19 20:02:02 +0000

    Added my personal solution

--- candidate-testing.js
+++ candidate-testing.js
@@ -62,7 +62,6 @@ function gradeQuiz(candidateAnswers) {
 }
 
 function runProgram() {
-  //askForName();
   // TODO 1.1c: Ask for candidate's name //
   console.log("Greetings", askForName(), "\n");
   askQuestion();
